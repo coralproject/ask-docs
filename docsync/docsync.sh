93d47bb50654cea64fcf8edc750f20b4c3f72901
David Jay <davidgljay@gmail.com>
2016-10-20 10:13:43 -0400

    Changing s3cmd sync to put
    This will ensure that css files are given the appropriate mimetypes.

--- docsync/docsync.sh
+++ docsync/docsync.sh
@@ -4,5 +4,4 @@ cd ./repo
 mkdocs build --clean
 cd $SUBDIR
 s3cmd -rf --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US rm s3://$S3_BUCKET
-s3cmd -rPM --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US sync ./ s3://$S3_BUCKET
- 
+s3cmd -rPM --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US put ./ s3://$S3_BUCKET
