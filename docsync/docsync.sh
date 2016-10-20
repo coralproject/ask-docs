cd3f22a0cf4cc920aa99ce99360d6bd4648443bf
sfphoenix <simonfarleyisthephoenix@gmail.com>
2016-10-19 18:40:04 -0700

    reorder docsync.sh commands

--- docsync/docsync.sh
+++ docsync/docsync.sh
@@ -1,7 +1,7 @@
 . ./config.sh
-s3cmd -rf --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US rm s3://$S3_BUCKET
 git clone $GITHUB_REPO ./repo
 cd ./repo
 mkdocs build --clean
 cd $SUBDIR
+s3cmd -rf --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US rm s3://$S3_BUCKET
 s3cmd -rPM --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US sync ./ s3://$S3_BUCKET
