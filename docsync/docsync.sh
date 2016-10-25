2b15757ee692d19d05da231385659eaf78f54feb
sfphoenix <simonfarleyisthephoenix@gmail.com>
2016-10-24 22:14:46 -0700

    workflow

--- docsync/docsync.sh
+++ docsync/docsync.sh
@@ -3,5 +3,5 @@ git clone $GITHUB_REPO ./repo
 cd ./repo
 mkdocs build --clean
 cd $SUBDIR
-s3cmd -rf --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US rm s3://$S3_BUCKET
-s3cmd -rPM --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY --region=US put ./ s3://$S3_BUCKET
+s3cmd del -r --force --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY s3://$S3_BUCKET
+s3cmd put ./* -rM --force --no-mime-magic --guess-mime-type --access_key=$AWS_ACCESS_KEY --secret_key=$AWS_SECRET_KEY s3://$S3_BUCKET 
